55f09092d6518a7d953ff12db86c470e2fb8b172
DPPetrov <103362620+DPPetrov@users.noreply.github.com>
2022-05-18 23:36:30 +0300

    Add files via upload

---
+++ ForeignLanguages.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ForeignLanguages {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        var input = sc.nextLine();
+
+        switch (input){
+            case "USA":
+            case "England":
+                System.out.println("English");
+                break;
+            case "Spain":
+            case "Argentina":
+            case "Mexico":
+                System.out.println("Spanish");
+                break;
+            default:
+                System.out.println("unknown");
+                break;
+        }
+    }
+}
